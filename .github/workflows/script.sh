ee510838d8d67c91f84d5aaa5789c3ec79177299
Satya Dwivedi <satyadwivedi@Satyas-MacBook-Air.local>
2025-02-27 16:31:30 +0530

    test

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -20,10 +20,10 @@ npx mbt build --mtar app.mtar
 
 echo 'Login BTP'
 echo cf api $cf_api_url
-cf login -a $cf_api_url -u $cf_user -p $cf_password
+#cf login -a $cf_api_url -u $cf_user -p $cf_password
 #cf auth $cf_user "$cf_password"
 #cf login -a https://api.cf.us20.hana.ondemand.com -u sadwivedi@suncor.com -p 'asdsdsf' -o Suncor_Energy_Services_Inc_Attn:_Accounts_Payable_clfdev01 -s DEV 
-#cf login -a https://api.cf.us20.hana.ondemand.com -o Suncor_Energy_Services_Inc_Attn:_Accounts_Payable_clfdev01 -s DEV --sso-passcode s5t0gXTA54S2_uL7j2dPVNAX7OMAZ8XC
+cf login -a https://api.cf.us20.hana.ondemand.com -o Suncor_Energy_Services_Inc_Attn:_Accounts_Payable_clfdev01 -s DEV --sso-passcode FulBj2fw81Y-tInEaClhRKJkFoxdNjfj
 
 echo 'Deploy the app'
 #cf target -o $cf_org -s $cf_space
