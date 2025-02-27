fc4d96529cd38c4aa8b2fe939a9da6de615a7cbd
Satya Dwivedi <satyadwivedi@Satyas-MacBook-Air.local>
2025-02-27 16:49:45 +0530

    btp login issue

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -19,7 +19,7 @@ echo 'Build the project'
 npx mbt build --mtar app.mtar
 
 echo 'Login BTP'
-echo cf api $cf_api_url
+#echo cf api $cf_api_url
 #cf login -a $cf_api_url -u $cf_user -p $cf_password
 #cf auth $cf_user "$cf_password"
 #cf login -a https://api.cf.us20.hana.ondemand.com -u sadwivedi@suncor.com -p 'asdsdsf' -o Suncor_Energy_Services_Inc_Attn:_Accounts_Payable_clfdev01 -s DEV 
